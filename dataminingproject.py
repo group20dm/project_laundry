20c1ecd0d3c14cc3cb4697ebc13177a2af6f7aa3
group20dm <121847508+group20dm@users.noreply.github.com>
2023-01-03 14:55:47 +0800

    Update dataminingproject.py

--- dataminingproject.py
+++ dataminingproject.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.header('walao')
+#first page data analysis (skip)
+#left side google map
+
+#second page feature selection & smote
+#left sub header boruta right is rfe
+
+#third page model
