c6a5d4d681f866c876770319ebdab163bbe07eaf
group20dm <121847508+group20dm@users.noreply.github.com>
2023-01-04 14:57:26 +0800

    Update dataminingproject.py

--- dataminingproject.py
+++ dataminingproject.py
@@ -1,6 +1,18 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-st.header('walao')
+with st.sidebar:
+  selected = option_menu (
+    menu_title = "Main menu",
+    options = ["Data Analysis", "Feature Selection & SMOTE", "Model"],
+  )
+ 
+if selected == "Data Analysis":
+  st.title("Data Analysis")
+if selected == "Feature Selection & SMOTE":
+  st.title("Feature Selection & SMOTE")
+if selected == "Model":
+  st.title("Model")
 #first page data analysis (skip)
 #left side google map
 
