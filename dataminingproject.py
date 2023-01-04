eaafd165452784cea3425da24b643555e5316cd5
group20dm <121847508+group20dm@users.noreply.github.com>
2023-01-04 16:03:57 +0800

    Update dataminingproject.py

--- dataminingproject.py
+++ dataminingproject.py
@@ -1,16 +1,20 @@
 import streamlit as st
 import pandas as pd
+import folium
+
+from folium.plugins import HeatMap
 from streamlit_option_menu import option_menu
 
 st.set_page_config(layout="wide")
 
+#navbar
 with st.sidebar:
   selected = option_menu (
     menu_title = "Main menu",
     options = ["Data Analysis", "Feature Selection & SMOTE", "Model"],
     icons = ["bar-chart-line","box-arrow-down-right","robot"],
   )
- 
+#first page
 if selected == "Data Analysis":
   st.title("Data Analysis")
   
@@ -18,17 +22,21 @@ if selected == "Data Analysis":
   df = pd.read_csv('dataset.csv')
   st.dataframe(df)
   
+  #columns
   col1, col2 = st.columns(2)
   with col1:
     st.header("Google map")
     st.map(df)
+    HeatMap(df)
   with col2:
     st.header("2nd col")
     st.image("https://static.streamlit.io/examples/cat.jpg")
-  
+    
+#second page
 if selected == "Feature Selection & SMOTE":
   st.title("Feature Selection & SMOTE")
   
+  #columns
   col1, col2 = st.columns(2)
   with col1:
     st.header("Boruta")
@@ -37,6 +45,7 @@ if selected == "Feature Selection & SMOTE":
     st.header("RFE")
     st.image("https://static.streamlit.io/examples/cat.jpg")
     
+#third page
 if selected == "Model":
   st.title("Model")
 #first page data analysis (skip)
