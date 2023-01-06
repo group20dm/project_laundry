7fb695c95d85fc5c4e075133256242dea5abdea8
group20dm <121847508+group20dm@users.noreply.github.com>
2023-01-06 16:12:14 +0800

    Update dataminingproject.py

--- dataminingproject.py
+++ dataminingproject.py
@@ -43,8 +43,8 @@ if selected == "Data Analysis":
     HeatMap(heat_data).add_to(map_heatmap)
     st_folium(map_heatmap)
     
-    msia_geojson=geopandas.read_file('project_laundry/stanford-zd362bc5680-geojson.json')
-    msia_geojson.plot()
+#     msia_geojson=geopandas.read_file('project_laundry/stanford-zd362bc5680-geojson.json')
+#     msia_geojson.plot()
     
   with col2:
     st.header("2nd col")
