369067ad84b1ec08332fbef52ac50926a1bd8992
group20dm <121847508+group20dm@users.noreply.github.com>
2023-01-07 17:54:19 +0800

    Update dataminingproject.py

--- dataminingproject.py
+++ dataminingproject.py
@@ -2,7 +2,10 @@ import streamlit as st
 import pandas as pd
 import folium
 import geopandas
+import plotly.express as px
+import plotly.graph_objects as go
 
+from plotly.subplots import make_subplots
 from streamlit_folium import st_folium
 from folium.plugins import HeatMap, FastMarkerCluster
 from streamlit_option_menu import option_menu
@@ -25,7 +28,7 @@ if selected == "Data Analysis":
   st.dataframe(df)
   
   #columns
-  col1, col2 = st.columns(2)
+  col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
   with col1:
     st.header("Google map")
     map_heatmap = folium.Map(location=[3.060525411,101.6105832], zoom_start=11)
@@ -46,7 +49,13 @@ if selected == "Data Analysis":
     
   with col2:
     st.header("2nd col")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
+    days_count = data.groupby("date").size().reset_index()
+
+    days_count.columns = ["date", "total_cust"]
+
+    fig = px.line(days_count, x = "date", y = "total_cust")
+    fig.update_layout(title = "Total Number of Customers in Each Days", xaxis_title = "Date", yaxis_title = "Total Number of Customers")
+    fig.show()
     
 #second page
 if selected == "Feature Selection & SMOTE":
