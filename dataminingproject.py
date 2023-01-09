40d529493809f56bf24b5679addfd6e34e9243c6
group20dm <121847508+group20dm@users.noreply.github.com>
2023-01-09 19:12:43 +0800

    Update dataminingproject.py

--- dataminingproject.py
+++ dataminingproject.py
@@ -41,7 +41,7 @@ if selected == "Data Analysis":
   col1, col2 = st.columns([1,2])
   with col1:
     st.header("Google map")
-    map_heatmap = folium.Map(location=[3.060525411,101.6105832], zoom_start=11,height = 20, width = 20)
+    map_heatmap = folium.Map(location=[3.060525411,101.6105832], zoom_start=11,height = 5, width = 5)
     
     # Filter the DF for columns, then remove NaNs
     heat_data = data[["latitude", "longitude"]]
