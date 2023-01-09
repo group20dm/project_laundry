bc14d5aeec9d8d5a2396e32af9b13d9d73464cef
group20dm <121847508+group20dm@users.noreply.github.com>
2023-01-09 18:32:12 +0800

    Update dataminingproject.py

--- dataminingproject.py
+++ dataminingproject.py
@@ -38,7 +38,7 @@ if selected == "Data Analysis":
   col1, col2, col3, col4 = st.columns(4)
   with col1:
     st.header("Google map")
-    map_heatmap = folium.Map(location=[3.060525411,101.6105832], zoom_start=13,height = 50, width = 50)
+    map_heatmap = folium.Map(location=[3.060525411,101.6105832], zoom_start=11,height = 50, width = 50)
     
     # Filter the DF for columns, then remove NaNs
     heat_data = data[["latitude", "longitude"]]
@@ -63,7 +63,21 @@ if selected == "Data Analysis":
     fig = px.line(days_count, x = "date", y = "total_cust")
     fig.update_layout(title = "Total Number of Customers in Each Days", xaxis_title = "Date", yaxis_title = "Total Number of Customers")
     st.write(fig)
-    
+  
+  with col3:
+    sales = data.copy()
+    sales["month_year"] = sales.date.dt.strftime('%Y-%m')
+    sales = sales.groupby("month_year").totalspent_rm.sum().reset_index()
+    sales.columns = ["month_year", "sales"]
+
+    fig  = make_subplots(1, 2, specs = [[{"type": "domain"}, {"type": "xy"}]], 
+                        subplot_titles = ["Percentage of Sales<br>in Each Year and Month", "Total Sales in Each Year and Month"])
+    fig.add_trace(go.Pie(hole= .5, labels= sales.month_year, values= sales.sales, legendgroup = "1"), row = 1, col = 1)
+    fig.add_trace(go.Scatter(x = sales.month_year, y = sales.sales, showlegend = False, legendgroup = "2"), row = 1, col = 2)
+
+    fig.update_layout(margin = {"l": 0, "r": 0, "b": 0, "t": 50}, legend_title_text = "Year and Month", 
+                     yaxis1_title = "Year and Month", xaxis1_title = "Total Sales (RM)")
+    st.write(fig)
 #second page
 if selected == "Feature Selection & SMOTE":
   st.title("Feature Selection & SMOTE")
