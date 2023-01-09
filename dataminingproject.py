7f72865457d0fd912845ffd76bff7debbae967bb
group20dm <121847508+group20dm@users.noreply.github.com>
2023-01-09 18:57:01 +0800

    Update dataminingproject.py

--- dataminingproject.py
+++ dataminingproject.py
@@ -85,7 +85,7 @@ if selected == "Data Analysis":
     st.write(perc_sale_fig)
   
   with col2:
-    sns_heatmap, ax = plt.subplots(figsize=(5, 5))
+    sns_heatmap, ax = plt.subplots(figsize=(10, 10))
     sns.heatmap(data.corr(), annot = True, cmap = "YlGnBu", ax=ax)
     st.write(sns_heatmap) 
     
