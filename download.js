30ba4276f4db5ac35ff1144fff1cf3223ef43f6d
group20dm <121847508+group20dm@users.noreply.github.com>
2023-01-14 18:02:51 +0800

    Add files via upload

---
+++ download.js
@@ -0,0 +1,34 @@
+const stDoc = window.parent.document;
+
+const container = stDoc.querySelector(".main > .block-container");
+
+const buttons = Array.from(stDoc.querySelectorAll(".stButton > button"));
+const download_button = buttons.find(el => el.innerText === "Download PDF");
+const height = container.scrollHeight;
+const width = container.scrollWidth;
+
+let pdf_width = height + 20;
+let pdf_height = pdf_width * 1.5 + 20;
+
+let n_pages = Math.ceil(height / pdf_height) - 1;
+download_button.innerText = "Downloading PDF...";
+download_button.style.display = "none";
+
+html2canvas(container, {allowTaint: true}).then(function (canvas) {{
+
+    canvas.getContext('2d');
+
+    let imgData = canvas.toDataURL("temp/jpeg", 1.0);
+    let pdf = new jsPDF('p', 'px', [pdf_width, pdf_height]);
+    pdf.addImage(imgData, 'JPG', 10, 10, width, height);
+
+    for (let page = 1; page <= n_pages; page++) {{
+        pdf.addPage();
+        pdf.addImage(imgData, 'JPG', 10, -(pdf_height * page) + 40, width, height);
+    }}
+
+    pdf.save('test.pdf');
+
+    download_button.innerText = "Download PDF";
+    download_button.style.display = "block";
+}})
